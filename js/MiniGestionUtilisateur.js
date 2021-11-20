f0c3206b10704af54adbaaedccc47fb50075f80a
hediismail <hedi.ismail.tr.1990@gmail.com>
2021-11-20 13:34:41 +0100

    modification et ajout bouton supprimer

--- js/MiniGestionUtilisateur.js
+++ js/MiniGestionUtilisateur.js
@@ -1,72 +1,62 @@
 var boutonAddPrenom = document.getElementById("btn-add-user");
-boutonAddPrenom.addEventListener("click",function(){
-//Prénom
+boutonAddPrenom.addEventListener("click", function () {
+    //Prénom
     var input1 = document.getElementById("text-add-prenom");
     var prenom = input1.value.trim();
-    if(prenom.length == 0) return;
+    if (prenom.length == 0) return;
 
-    var item1 = document.createElement("li");
-    item1.setAttribute("class", "list-group-item");
-    item1.innerText = prenom;
-    var liste1 = document.getElementById("list-prenom");
-    liste1.appendChild(item1);
-
+    var tr = document.createElement("tr")
+    tr.setAttribute("class", "lig")
+    var td1 = document.createElement("td");
+    td1.innerText = prenom;
+    tr.appendChild(td1)
+    var liste1 = document.getElementById('list');
     input1.value = '';
     input1.focus();
-        // Delete a li item on click 
-        liste1.onclick = function(){
-            this.parentNode.removeChild(this);
-          }
 
-//Nom
+    //Nom
     var input2 = document.getElementById("text-add-nom");
     var nom = input2.value.trim();
-    if(nom.length == 0) return;
-
-    var item2 = document.createElement("li");
-    item2.setAttribute("class", "list-group-item");
-    item2.innerText = nom;
-    var liste2 = document.getElementById("list-nom");
-    liste2.appendChild(item2);
-
+    if (nom.length == 0) return;
+    var td2 = document.createElement("td");
+    td2.innerText = nom;
+    tr.appendChild(td2)
     input2.value = '';
     input2.focus();
 
-//Mail
+    //Mail
     var input3 = document.getElementById("text-add-mail");
     var mail = input3.value.trim();
-    if(mail.length == 0) return;
+    if (mail.length == 0) return;
 
-    var item3 = document.createElement("li");
-    item3.setAttribute("class", "list-group-item");
-    item3.innerText = mail;
-    var liste3 = document.getElementById("list-mail");
-    liste3.appendChild(item3);
-
+    var td3 = document.createElement("td");
+    td3.innerText = mail;
+    tr.appendChild(td3);
     input3.value = '';
     input3.focus();
 
-//Poste
+    //Poste
     var input4 = document.getElementById("text-add-poste");
     var poste = input4.value.trim();
-    if(poste.length == 0) return;
-
-    var item4 = document.createElement("li");
-    item4.setAttribute("class", "list-group-item");
-    item4.innerText = poste;
-    var liste4 = document.getElementById("list-poste");
-    liste4.appendChild(item4);
-
+    if (poste.length == 0) return;
+    var td4 = document.createElement("td");
+    td4.innerText = poste;
+    tr.appendChild(td4);
     input4.value = '';
     input4.focus();
-});
 
-let bouton = document.getElementById("supp");
-bouton.addEventListener("click",function(){
-    let elems = document.getElementById("lig");
-    for(var i=0; i< elems.length; i++){
-        console.log(elems[i]);
-        alert(elems[i].innerText);
-        elems[i].remove();
-    }
+    // Bouton supprimer
+    let boutonSupp = document.createElement("button")
+    var td5 = document.createElement("td");
+    boutonSupp.setAttribute('class', "bouton-supprimer btn btn-primary btn-sm list-group-item")
+
+    boutonSupp.innerText = "Supp"
+    td5.appendChild(boutonSupp)
+    tr.appendChild(td5)
+
+    liste1.appendChild(tr);
+
+    boutonSupp.addEventListener("click", () => {
+        liste1.removeChild(tr)
+    })
 });
